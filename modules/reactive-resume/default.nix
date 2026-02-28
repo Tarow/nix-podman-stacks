711d69fd8c9f26db7197977a678690e784325d2b
Niklas Tasler <niklastasler@gmail.com>
2026-02-28 15:10:17 +0100

    chore: update image

--- modules/reactive-resume/default.nix
+++ modules/reactive-resume/default.nix
@@ -16,6 +16,8 @@
 in {
   imports = import ../mkAliases.nix config lib name [
     name
+    dbName
+    chromeName
   ];
 
   options.nps.stacks.${name} = {
@@ -111,7 +113,7 @@ in {
 
     services.podman.containers = {
       ${name} = {
-        image = "ghcr.io/amruthpillai/reactive-resume:v5.0.5";
+        image = "ghcr.io/amruthpillai/reactive-resume:v5.0.10";
         volumeMap = {
           data = "${storage}/data:/app/data";
         };
