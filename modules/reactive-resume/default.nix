303b562c59c9f6124bd532dda415c0d07a116812
Niklas Tasler <niklastasler@gmail.com>
2026-03-21 16:27:56 +0100

    chore: update image

--- modules/reactive-resume/default.nix
+++ modules/reactive-resume/default.nix
@@ -113,7 +113,7 @@ in {
 
     services.podman.containers = {
       ${name} = {
-        image = "ghcr.io/amruthpillai/reactive-resume:v5.0.10";
+        image = "ghcr.io/amruthpillai/reactive-resume:v5.0.13";
         volumeMap = {
           data = "${storage}/data:/app/data";
         };
