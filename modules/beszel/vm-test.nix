a2488faa62282f64a9d98d82dc276ba6c9c1b4ec
Niklas Tasler <niklastasler@gmail.com>
2026-09-13 14:26:44 +0200

    fix: beszel integration test

--- modules/beszel/vm-test.nix
+++ modules/beszel/vm-test.nix
@@ -1,16 +1,26 @@
 {
+  config,
+  pkgs,
   dummyClientSecretHash,
   dummySecretFile,
   ...
-}: {
+}: let
+  sshKeys =
+    pkgs.runCommand "beszel-test-ssh-keys" {
+      nativeBuildInputs = [pkgs.openssh];
+    } ''
+      mkdir -p $out
+      ssh-keygen -q -t ed25519 -N "" -C "beszel-test" -f $out/id_ed25519
+    '';
+in {
   imports = [
     ../authelia/vm-test.nix
     ../docker-socket-proxy/vm-test.nix
   ];
   nps.stacks.beszel = {
     enable = true;
-    ed25519PrivateKeyFile = dummySecretFile;
-    ed25519PublicKeyFile = dummySecretFile;
+    ed25519PrivateKeyFile = "${sshKeys}/id_ed25519";
+    ed25519PublicKeyFile = "${sshKeys}/id_ed25519.pub";
     tokenFile = dummySecretFile;
     adminProvisioning = {
       email = "admin@admin.com";
