0a08368ce07af9a496e9121dc8c4e3b6e60ed6a1
Niklas Tasler <niklastasler@gmail.com>
2026-09-13 14:51:09 +0200

    fix: kitchenowl integration test

--- modules/kitchenowl/vm-test.nix
+++ modules/kitchenowl/vm-test.nix
@@ -1,6 +1,7 @@
 {
   dummyHash,
   dummySecretFile,
+  selfSignedCertDir,
   ...
 }: {
   imports = [../authelia/vm-test.nix];
@@ -12,5 +13,18 @@
       clientSecretFile = dummySecretFile;
       clientSecretHash = dummyHash;
     };
+  };
+
+  # Kitchenowl's Python backend fetches Authelia's
+  # `.well-known/openid-configuration` over HTTPS at startup. Trust the test's
+  # self-signed wildcard certificate (Python `requests` uses REQUESTS_CA_BUNDLE,
+  # while `ssl.create_default_context` honours SSL_CERT_FILE).
+  services.podman.containers.kitchenowl-backend = {
+    extraEnv = {
+      REQUESTS_CA_BUNDLE = "/etc/ssl/certs/nps-test/wildcard.crt";
+      CURL_CA_BUNDLE = "/etc/ssl/certs/nps-test/wildcard.crt";
+      SSL_CERT_FILE = "/etc/ssl/certs/nps-test/wildcard.crt";
+    };
+    volumeMap.npsTestCert = "${selfSignedCertDir}:/etc/ssl/certs/nps-test:ro";
   };
 }
